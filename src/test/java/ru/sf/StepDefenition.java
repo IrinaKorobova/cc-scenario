3f9ac339fb26bde106c55f126896e45bbea9b4b4
Korobova Irina <korbovartem@gmail.com>
2022-04-21 12:58:12 +0500

    First commit

---
+++ src/test/java/ru/sf/StepDefenition.java
@@ -0,0 +1,23 @@
+package ru.sf;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class StepDefenition {
+    @Given("url of NL {string}")
+    public void url_of_nl(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("open online store")
+    public void open_online_store() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("website is open start search {string}")
+    public void website_is_open_start_search(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+}
---
+++ target/test-classes/ru.sf/scenario.feature
@@ -0,0 +1,8 @@
+Feature: search bar
+  Scenario: product search
+    Given url of NL 'https://www.nlstar.com/ru/'
+    When open online store
+    Then website is open start search 'UP2U'
+
+
+
