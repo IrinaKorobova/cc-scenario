a717e6de19442ee237c546ddc1bf56ec55e8c53b
Korobova Irina <korbovartem@gmail.com>
2022-04-23 18:27:49 +0500

    2 commit

--- src/test/java/ru/sf/StepDefenition.java
+++ src/test/java/ru/sf/StepDefenition.java
@@ -3,21 +3,44 @@ package ru.sf;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
 
 public class StepDefenition {
-    @Given("url of NL {string}")
-    public void url_of_nl(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
+    private static final WebDriver webdriver;
+    static {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\User\\IdeaProjects\\сс-scenario\\src\\test\\resources\\chromedriver.exe");
+        webdriver =  new ChromeDriver();
     }
+
+    @Given("url of NL {string}")
+    public void url_of_nl(String url) {
+        webdriver.get(url);
+    }
+
     @When("open online store")
     public void open_online_store() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        webdriver.findElement(By.className("info-buttons__item")).click();
     }
     @Then("website is open start search {string}")
-    public void website_is_open_start_search(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    public void website_is_open_start_search(String searchParam) {
+        WebElement searchField = webdriver.findElement(By.className("ns-input"));
+        searchField.sendKeys(searchParam);
+        searchField.submit();
     }
+    @Then("assert that user got message {string}")
+    public void assert_that_user_got_message(String errorMassage) {
+        final var actualMassage = webdriver.findElement(By.className("base-title")).getText();
+        Assert.assertEquals(errorMassage, actualMassage);
+    }
+
+
 }
--- target/test-classes/ru.sf/scenario.feature
+++ target/test-classes/ru.sf/scenario.feature
@@ -3,6 +3,13 @@ Feature: search bar
     Given url of NL 'https://www.nlstar.com/ru/'
     When open online store
     Then website is open start search 'UP2U'
+  Scenario: search for non-existent products
+    Given  url of NL 'https://www.nlstar.com/ru/'
+    When open online store
+    Then website is open start search 'U2'
+    And assert that user got message 'Простите, по вашему запросу ничего не найдено.'
+
+
 
 
 
